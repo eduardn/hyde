17000b4c4e89960e16a32ef89dfcb740aa40e870
Eduard Neculaesi <neculaesi.eduard@gmail.com>
2021-09-25 22:18:35 +0300

    Init hyde project

---
+++ packages/core/src/hyde.js
@@ -0,0 +1,3 @@
+const hyde = {};
+
+export default hyde;
---
+++ packages/core/test/hyde.test.js
@@ -0,0 +1,8 @@
+import hyde from '../src/hyde';
+import { expect } from 'chai';
+
+describe('hyde', () => {
+  it('should export an object', () => {
+    expect(hyde).to.be.an('object');
+  });
+});
